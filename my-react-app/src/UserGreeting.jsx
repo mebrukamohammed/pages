095e3a5061afcbe9e3cc79478e040d330edb352b
Mebruka <mebrukaaa7612@gmail.com>
2026-09-23 15:38:01 +0300

    Delete my-react-app directory

--- my-react-app/src/UserGreeting.jsx
+++
@@ -1,9 +0,0 @@
-function UserGreeting(props){
-       return (props.isLoggedIn ? <h2 className="logged-in">welcome{props.name}</h2> :
-                           <h2 className="not-logged-in">please login first</h2>)
-        
-       
-    
-
-}
-export default UserGreeting
